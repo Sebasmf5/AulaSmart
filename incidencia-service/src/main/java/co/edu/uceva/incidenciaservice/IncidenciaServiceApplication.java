836720389a0ea758b2fbbc198830607f69c4a9fa
sebasmf <198938779+angela-aponte@users.noreply.github.com>
2026-03-30 11:15:08 -0500

    chore: create project and add dependencies in pom.xml

---
+++ incidencia-service/src/main/java/co/edu/uceva/incidenciaservice/IncidenciaServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.incidenciaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidenciaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidenciaServiceApplication.class, args);
+    }
+
+}
---
+++ incidencia-service/src/test/java/co/edu/uceva/incidenciaservice/IncidenciaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.incidenciaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidenciaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
