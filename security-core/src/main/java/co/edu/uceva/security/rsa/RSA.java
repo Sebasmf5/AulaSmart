b69ca07d7283417f6d78b3f4e5652cb11b95adca
stevendz2002 <jhon.loaiza01@uceva.edu.co>
2026-03-31 00:37:36 -0500

    AUS-42 Cifrado AES y RSA

--- security-core/src/main/java/co/edu/uceva/security/rsa/RSA.java
+++ security-core/src/main/java/co/edu/uceva/security/rsa/RSA.java
@@ -1,4 +1,19 @@
 package co.edu.uceva.security.rsa;
 
-public class rsa {
+
+public class RSA {
+
+    public static long modExp(long base, long exp, long mod) {
+        long result = 1;
+        base = base % mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            exp = exp >> 1;
+            base = (base * base) % mod;
+        }
+        return result;
+    }
+
 }
